1455151402aab0cd27a631cd8c3064a633de3d19
martvanvliet123 <m.vanvliet@tilburguniversity.edu>
2025-02-17 14:38:30 +0100

    Create conversierates.R

---
+++ conversierates.R
@@ -0,0 +1,5 @@
+# Lijst van conversierates
+dropout <- 0.00             #mensen die niet naar OK moeten na poli
+conversierate_poli <- 0.00  #mensen die van poli rechtstreeks naar hosp gaan
+conversierate_surg <- 1.00  #mensen die van poli naar OK moeten
+conversierate_po_po <- 0.00   #mensen die na poli nog een poli moeten
